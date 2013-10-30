e163f879221a2aa35e2e281423307ca4aa59cc25
Rafael Grillo <grillorafael@gmail.com>
2013-10-30 16:09:07 -0200

    Update spree_static_content.js

--- app/assets/javascripts/admin/spree_static_content.js
+++ app/assets/javascripts/admin/spree_static_content.js
@@ -4,7 +4,6 @@
 $(function(){
   var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('page_body'), {
     lineWrapping: true,
-    mode: 'text/html',
     lineNumbers: true
   });
 });
